90978cbe410c1ba6ed3e3747e4e21302b59282eb
Eden Kaduri <edenkadouri@gmail.com>
2022-02-14 18:18:50 +0200

    Added mongo connection with schemas

--- Server/app.js
+++ Server/app.js
@@ -1,35 +1,63 @@
 var express = require('express')
 var app = express()
-var fs = require('fs');
-// var socket = require('socket.io')
-const ads_1 = require('./ads/ads_1.json')
-const ads_2 = require('./ads/ads_2.json')
-const ads_3 = require('./ads/ads_3.json')
-// var future = document.getElementById("future");
-// var io = socket.listen(app)
+const process = require('process')
+const args = process.argv
+const userName = args[2]
+const pass = args[3]
+
+
+const Messagedb = require('./schemas_models/Ads');
+const Admindb = require('./schemas_models/Admin');
+
+var mongoose = require('mongoose');
+var mongoDB = "mongodb+srv://" + userName + ':' + pass + "@cluster0.6ykyw.mongodb.net/Ads_DB?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(makeDB())
+    .then(() => app.listen(8080))
+    .then(console.log("connected to mongo and server listening on port 8080"));
+
 
 app.use(express.static(__dirname + '/node_modules'));
-app.get('/screen=:id', function (req, res) {
-    const id = req.params.id
-    var template = ""
-    var ads = null
-    switch (id) {
-        case '1':
-            ads = ads_1;
-            template = "template1"
-            break;
-        case '2':
-            ads = ads_2;
-            template = "template2"
-            break;
+// app.get('/screen=:id', function (req, res) {
+//     const id = req.params.id
+//     var template = ""
+//     var ads = null
+//     switch (id) {
+//         case '1':
+//             ads = ads_1;
+//             template = "template1"
+//             break;
+//         case '2':
+//             ads = ads_2;
+//             template = "template2"
+//             break;
 
-    }
-    // const html = fs.readFileSync(`./ads/Templates/` + template + `.html`, 'utf8')
-    // res.json({ html: html, ads: ads })רקד
-    res.sendFile(__dirname + `/` + template + `.html`);
-});
+//     }
+//     // const html = fs.readFileSync(`./ads/Templates/` + template + `.html`, 'utf8')
+//     // res.json({ html: html, ads: ads })רקד
+//     res.sendFile(__dirname + `/` + template + `.html`);
+
+// });
 
 
-app.listen(4200, function () {
-    console.log("server is listening on port 4200...");
-});
+
+
+
+
+
+async function makeDB() {
+
+    await Admindb.deleteMany({});
+    await Messagedb.deleteMany({});
+
+
+    Admindb.create({ userName: "eden", passWord: "123" });
+    Admindb.create({ userName: "hen", passWord: "123" });
+    Admindb.create({ userName: "bar", passWord: "123" });
+
+
+
+    const msgArr = require('./Ads.json');
+    Messagedb.insertMany(msgArr);
+    return;
+}
